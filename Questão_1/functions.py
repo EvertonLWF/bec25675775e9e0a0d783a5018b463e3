45a323bf5018e50aa1b787c85bbdab8bd22db75c
Everton Feijo <evertonlwf@gmail.com>
2022-11-13 00:16:07 -0300

    ajustes na função move

--- Questão_1/functions.py
+++ Questão_1/functions.py
@@ -16,9 +16,9 @@ def DimensionsValidation(dimensions):
 def Move(move, dimensions, initial_position, currentDirection):
     direction = currentDirection
     position = initial_position
+    dimensions = dimensions.split(" ")
     for i in move:
         if(i == "F"):
-            print("move forward")
             if(direction == "N"):
                 # se a direção for norte e a posição y for menor que a largura do ambiente, a posição é incrementada.
                 if(position[1] < int(dimensions[1]) - 1):
@@ -36,8 +36,6 @@ def Move(move, dimensions, initial_position, currentDirection):
                 if(position[0] > 0):
                     position[0] -= 1
         elif(i == "T"):
-            #back
-            print("back")
             if(direction == "N"):
                 # se a direção for norte e a posição y for maior que 0, a posição é decrementada.
                 if(position[1] > 0):
@@ -54,7 +52,7 @@ def Move(move, dimensions, initial_position, currentDirection):
                 # se a direção for oeste e a posição x for menor que o comprimento do ambiente, a posição é incrementada.
                 if(position[0] < int(dimensions[0]) - 1):
                     position[0] += 1
-        elif(i == "L"):
+        elif(i == "E"):
             # se a direção for norte, a direção é alterada para oeste.
             if(direction == "N"):
                 direction = "O"
@@ -67,11 +65,19 @@ def Move(move, dimensions, initial_position, currentDirection):
             # se a direção for leste, a direção é alterada para norte.
             elif(direction == "L"):
                 direction = "N"
-        elif(i == "R"):
-            #rotaciona para a direita
+        elif(i == "D"):
+            # se a direção for norte, a direção é alterada para leste.
             if(direction == "N"):
                 direction = "L"
+            # se a direção for leste, a direção é alterada para sul.
             elif(direction == "L"):
                 direction = "S"
+            # se a direção for sul, a direção é alterada para oeste.
             elif(direction == "S"):
                 direction = "O"
+            # se a direção for oeste, a direção é alterada para norte.
+            elif(direction == "O"):
+                direction = "N"
+                
+    
+    return direction + " " + str(position[0]) + " " + str(position[1])
--- Questão_1/main.py
+++ Questão_1/main.py
@@ -1,17 +1,17 @@
+import sys
 import functions
 
 #Aspirador Inteligente.
-
-input("Bem vindo ao aspirador inteligente, pressione enter para continuar")
+print("Bem vindo ao aspirador inteligente.")
 
 # Entrada de dados das dimensões do ambiente largura e comprimento.
-#dimensions = input("\nInforme as dimensões do ambiente: (ex: 3 3)\n").split(" ")
-dimensions = "3 3"
+dimensions = input("\nInforme as dimensões do ambiente: (ex: 3 3)\n")
+
+# Posição inicial do aspirador.
 currentDirection = "N"
 
 # Entrada de dados da sequencia de movimentos.
-#move = input("\nInforme a sequencia de movimentos: (ex: FDFEFDF)\n")
-move = "FDFEFDFEFDFEFDF"
+move = input("\nInforme a sequencia de movimentos: (ex: FDFEFDF)\n")
 
 # Entrada de dados da posição inicial do aspirador.
 initial_position = [0, 0]
@@ -19,12 +19,18 @@ initial_position = [0, 0]
 # Verifica se a sequencia de movimentos é valida.
 if(functions.MoveValidation(move) == False):
     print("Verifique a sequencia de movimentos, ela deve conter apenas os caracteres F, D, E e T.")
+    sys.exit()
 else:
     print("Os Movimentos são: ", move)
     
 # Verifica se as dimensões do ambiente são validas.
 if(functions.DimensionsValidation(dimensions) == False):
     print("Verifique as dimensões do ambiente, elas devem conter apenas números, separados por um espaço.")
+    sys.exit()
 else:
     print("As dimensões do ambiente são: ", dimensions)
+    
+# Executa a sequencia de movimentos e retorna o resultado.
+result = functions.Move(move, dimensions, initial_position, currentDirection)
 
+print("A posição final do aspirador é: ", result)
